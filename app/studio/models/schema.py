731e0a2e77223b397c5aaffd4819c45ca0cec288
Alex Bouenard <alexandre.bouenard@lennar.com>
2025-03-26 08:59:49 -0400

    - added more tests

--- app/studio/models/schema.py
+++ app/studio/models/schema.py
@@ -18,19 +18,38 @@ def read_dict(file_name: str) -> dict:
         return json.loads(content)
 
 
+def get_schema_dir() -> str:
+    current_dir = os.path.dirname(__file__)
+    return join_path(current_dir, '..', '..', 'data', 'schemas')
+
+
+def get_schema_file_name(version: str) -> str:
+    current_dir = os.path.dirname(__file__)
+    schemas_dir = join_path(current_dir, '..', '..', 'data', 'schemas')
+
+    return join_path(schemas_dir, f'{version}.json')
+
+
 class SchemaManager(models.Manager):
     def create(self, version: str):
-        current_dir = os.path.dirname(__file__)
-        schemas_dir = join_path(current_dir, '..', '..', 'data', 'schemas')
+        if version not in SchemaManager.get_supported_versions():
+            raise ValueError(f'Schema version "{version}" is not supported')
 
-        schema_file_name = join_path(schemas_dir, f'{version}.json')
-        if not os.path.isfile(schema_file_name):
-            raise FileNotFoundError(f'Schema file does not exist for version "{version}"')
-
+        schema_file_name = get_schema_file_name(version)
         schema_data = read_dict(schema_file_name)
         schema = Schema(schema_data=schema_data)
 
         return schema
+
+    @staticmethod
+    def get_supported_versions() -> [str]:
+        versions = []
+        for _, _, file_names in os.walk(get_schema_dir()):
+            for file_name in file_names:
+                base_file_name, ext = os.path.splitext(file_name)
+                if ext == '.json':
+                    versions.append(base_file_name)
+        return versions
 
 
 class Schema(models.Model):
--- app/studio/tests/test_schema.py
+++ app/studio/tests/test_schema.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
 
-from studio.models import Schema
+from studio.models import Schema, SchemaManager
 
 
 class TestSchema(TestCase):
-    def test_schema_creation(self):
-        schema = Schema.objects.create('1.0.0')
+    def test_valid_schemas(self):
+        versions = SchemaManager.get_supported_versions()
+        for version in versions:
+            schema = Schema.objects.create(version)
+            self.assertIsNotNone(schema.schema_data)
 
-        self.assertIsNotNone(schema.schema_data)
+    def test_invalid_schema(self):
+        with self.assertRaises(ValueError):
+            Schema.objects.create('x.y.z')
