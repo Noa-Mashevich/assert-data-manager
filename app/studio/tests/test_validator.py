ff900f49d76825b70daeea6eb3de555d16534b41
Alex Bouenard <alexandre.bouenard@lennar.com>
2025-03-27 08:55:18 -0400

    - added test

--- app/studio/tests/test_validator.py
+++ app/studio/tests/test_validator.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 
 from studio.views.validator import ValidatorViewSet
+from studio.file_utils import FileUtils
+
+from .utils import TestUtils
 
 
 class TestValidatorApi(TestCase):
@@ -22,3 +25,10 @@ class TestValidatorApi(TestCase):
         with self.assertRaises(Exception):
             request = self.factory.post(f'/validator', {'version': '1.0.0'})
             view(request)
+
+        data_file_name = TestUtils.get_data_path('test_schema_v1.0.0_2.json', 'schema')
+        data = FileUtils.read_dict(data_file_name)
+        request = self.factory.post(f'/validator', data)
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
