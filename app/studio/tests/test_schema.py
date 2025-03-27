9a476ebafc3b8ec4fa388738068a1d92e4363a08
Alex Bouenard <alexandre.bouenard@lennar.com>
2025-03-27 10:22:17 -0400

    - added test

--- app/studio/tests/test_schema.py
+++ app/studio/tests/test_schema.py
@@ -31,3 +31,10 @@ class TestSchema(TestCase):
 
         self.assertEqual(success, False)
         self.assertGreater(len(error_message), 0)
+
+        data_file_name = TestUtils.get_data_path('test_schema_v1.0.0_2.json', 'schema')
+        data = FileUtils.read_dict(data_file_name)
+        success, error_message = schema.validate(data)
+
+        self.assertEqual(success, True)
+        self.assertEqual(len(error_message), 0)
