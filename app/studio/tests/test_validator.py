e56c4481c5f6fe60e4f886265277dd8a9da1eccd
Alex Bouenard <alexandre.bouenard@lennar.com>
2025-03-27 16:08:49 -0400

    - fixed

--- app/studio/tests/test_validator.py
+++ app/studio/tests/test_validator.py
@@ -19,14 +19,20 @@ class TestValidatorApi(TestCase):
         self.assertEqual(response.status_code, 400)
 
         response = self.client.post(url, data='{}', content_type='application/json')
+        self.assertEqual(response.content, b'Missing version in input data')
 
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.content, 'Missing version in input data')
+        self.assertEqual(response.content, b'Missing version in input data')
 
         response = self.client.post(
             url, data='{"version": "1.0.0"}', content_type='application/json'
         )
+
         self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.content[0:86],
+            b'Failed validating data, version "1.0.0", reason: \'corePlanName\' is a required property',
+        )
 
         data_file_name = TestUtils.get_data_path('test_schema_v1.0.0_2.json', 'schema')
         data = FileUtils.read_content(data_file_name)
