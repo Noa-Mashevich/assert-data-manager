d9f8a2e0589939b460d850ed08dd37ef616953e3
Alex Bouenard <alexandre.bouenard@lennar.com>
2025-03-27 16:01:59 -0400

    - fixed linting

--- app/studio/tests/test_validator.py
+++ app/studio/tests/test_validator.py
@@ -23,8 +23,9 @@ class TestValidatorApi(TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.content, 'Missing version in input data')
 
-        response = self.client.post(url, data='{"version": "1.0.0"}', content_type='application/json')
-
+        response = self.client.post(
+            url, data='{"version": "1.0.0"}', content_type='application/json'
+        )
         self.assertEqual(response.status_code, 400)
 
         data_file_name = TestUtils.get_data_path('test_schema_v1.0.0_2.json', 'schema')
