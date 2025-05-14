aa2a10fe05af0abe2bcdef06ec6439e331801bb8
Alex Bouenard <166079873+ABouenard-Lennar@users.noreply.github.com>
2025-05-14 11:31:19 -0400

    - fixed (#23)

--- app/server/settings/base.py
+++ app/server/settings/base.py
@@ -89,5 +89,6 @@ AWS_REGION = 'us-west-2'
 AWS_CLIENT_S3 = boto3.client('s3', region_name=AWS_REGION)
 AWS_CLIENT_SQS = boto3.client('sqs', region_name=AWS_REGION)
 AWS_CLIENT_CFN = boto3.client('cloudformation', region_name=AWS_REGION)
+AWS_CLIENT_LAMBDA = boto3.client('lambda', region_name=AWS_REGION)
 
 AWS_STACKS = []
