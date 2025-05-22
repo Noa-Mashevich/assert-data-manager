246c315c09acd9df9402772b997c8a1835e42231
Alex Bouenard <166079873+ABouenard-Lennar@users.noreply.github.com>
2025-05-22 11:30:15 -0400

    - fixed (#37)

--- app/server/settings/base.py
+++ app/server/settings/base.py
@@ -79,6 +79,7 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
     'COMPONENT_SPLIT_REQUEST': True,
+    'SORT_OPERATIONS': False,
     'SERVERS': [
         {'url': 'http://localhost:8000', 'description': 'Local Development server'},
         {'url': 'https://dev.modsy.com', 'description': 'Development server'},
