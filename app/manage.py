175c8e10ecf6288553f18e82b14b970fee5f2bdf
akmodsy <adam.konrad@lennar.com>
2025-06-05 20:47:22 +0200

    prefixed environments (#42)

--- app/manage.py
+++ app/manage.py
@@ -9,7 +9,7 @@ load_dotenv("./.env")
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.veev-local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
