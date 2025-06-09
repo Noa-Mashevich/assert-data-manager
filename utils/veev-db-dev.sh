c0995a918fce767b45c8d0bacdf38b01ef4563b8
Adam Konrad <adam.konrad@lennar.com>
2025-06-09 20:56:27 +0200

    veev infra adjustments

--- utils/veev-db-dev.sh
+++ utils/veev-db-dev.sh
@@ -2,7 +2,7 @@
 
 ENVIRONMENT="dev"
 SNAPSHOT_ARN=""
-PARAMETERS="VpcId=vpc-0472ea68c16693fcf PrivateSubnets=subnet-0042ba85a0b0681e7,subnet-01a1227b58daca793 DeploymentEnv=${ENVIRONMENT} Database=studio InstanceType=db.t4g.small Storage=5 HighlyAvailable=false SnapshotARN=${SNAPSHOT_ARN}"
+PARAMETERS="VpcId=vpc-0472ea68c16693fcf PrivateSubnets=subnet-05c667ecc5fccd18d,subnet-0e2e4fa350c535033 DeploymentEnv=${ENVIRONMENT} Database=studio InstanceType=db.t4g.small Storage=5 HighlyAvailable=false SnapshotARN=${SNAPSHOT_ARN}"
 
 echo "build"
 sam build \
